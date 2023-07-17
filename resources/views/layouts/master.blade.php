6fc9ae2442dbf168b7959eda7f27608871c7947d
yilmaz <ydaskafa@gmail.com>
2023-07-17 03:16:09 +0300

    Favicon added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Insider Online Trial Day Project</title>
+    <link rel="shortcut icon" href="{{ url('assets/useinsider-favicon.webp') }}" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
